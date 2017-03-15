289983e0fb64ec565092eef930cffd245cb1d9d4
Adam Roses Wight <awight@wikimedia.org>
2017-03-15 12:42:26 -0700

    Can output to file

--- job_wrapper.py
+++ job_wrapper.py
@@ -1,5 +1,8 @@
+from __future__ import print_function
+import datetime
 import shlex
 import subprocess
+import sys
 import threading
 import yaml
 
@@ -12,6 +15,7 @@ class JobWrapper(object):
     def __init__(self, config_path=None):
         self.config = yaml.safe_load(file(config_path, "r"))
         self.name = self.config["name"]
+        self.start_time = datetime.datetime.utcnow().isoformat()
         lock.begin(job_tag=self.name)
 
     def run(self):
@@ -22,12 +26,45 @@ class JobWrapper(object):
         else:
             timeout = DEFAULT_TIMEOUT
 
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        timer = threading.Timer(timeout, process.kill)
+        self.process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        timer = threading.Timer(timeout, self.process.kill)
 
         try:
             # FIXME: This doesn't stream, so large output will be buffered in memory.
-            (stdout_data, stderr_data) = process.communicate()
+            (stdout_data, stderr_data) = self.process.communicate()
+
+            self.store_job_output(stdout_data)
+
+            if stderr_data != '':
+                self.fail_has_stderr(stderr_data)
         finally:
             timer.cancel()
             lock.end()
+
+        return_code = self.process.returncode
+        if return_code != 0:
+            self.fail_exitcode(return_code)
+
+    def fail_exitcode(self, return_code):
+        print("Job {name} failed with code {code}".format(name=self.name, code=return_code), file=sys.stderr)
+        # TODO: Prevent future jobs according to config.
+
+    def fail_has_stderr(self, stderr_data):
+        print("Job {name} printed things to stderr:".format(name=self.name), file=sys.stderr)
+        print(stderr_data, file=sys.stderr)
+
+    def store_job_output(self, stdout_data):
+        if "stdout_destination" not in self.config:
+            return
+
+        destination = self.config["stdout_destination"]
+        out = file(destination, "a")
+
+        header = (
+            "===========\n"
+            "{name} ({pid}), started at {time}\n"
+            "-----------\n"
+        ).format(name=self.name, pid=self.process.pid, time=self.start_time)
+        print(header, file=out)
+
+        out.write(stdout_data)
